ebca1b74b194b641f1554d423046cf7a1e8ac1e1
Max <silverhammermba@gmail.com>
2012-11-25 00:37:38 -0500

    Fix outdated method names in unit tests

--- test/System/Clock.rb
+++ test/System/Clock.rb
@@ -20,41 +20,41 @@ class TestClock < Test::Unit::TestCase
   
   def test_inspect
     clock = Clock.new
-    assert_match(/SFML::Clock\(\d+ms\)/, clock.inspect)
-    assert_match(/ SFML::Clock\(\d+ms\) /, " #{clock} ")
+    assert_match(/SFML::Clock\(\d+\.\d+s\)/, clock.inspect)
+    assert_match(/SFML::Clock\(\d+\.\d+s\)/, "#{clock}")
   end
   
   def test_elapsed_time
     clock = Clock.new
     sleep(0.100)
-    assert_in_delta(clock.time, 100, 40)
+    assert_in_delta(clock.time.as_milliseconds, 100, 40)
   end
   
-  def test_reset
+  def test_restart
     clock = Clock.new
-    assert_in_delta(clock.time, 0, 40)
+    assert_in_delta(clock.time.as_milliseconds, 0, 40)
     sleep(0.100)
-    assert_in_delta(clock.time, 100, 40)
+    assert_in_delta(clock.time.as_milliseconds, 100, 40)
     
-    clock.reset
-    assert_in_delta(clock.time, 0, 40)
+    clock.restart
+    assert_in_delta(clock.time.as_milliseconds, 0, 40)
     sleep(0.100)
-    assert_in_delta(clock.time, 100, 40)
+    assert_in_delta(clock.time.as_milliseconds, 100, 40)
   end
   
   def test_compare
     clock1 = Clock.new
     sleep(0.100)
     clock2 = Clock.new
-    assert_operator(clock1, :>, clock2)
-    refute_operator(clock1, :<=, clock2)
+    assert_operator(clock1.time, :>, clock2.time)
+    refute_operator(clock1.time, :<=, clock2.time)
     sleep(0.100)
-    assert_operator(clock1, :>, clock2)
-    refute_operator(clock1, :<=, clock2)
-    clock1.reset
-    assert_operator(clock1, :<=, clock2)
-    refute_operator(clock1, :>, clock2)
-    assert_operator(clock2, :>, clock1.time)
+    assert_operator(clock1.time, :>, clock2.time)
+    refute_operator(clock1.time, :<=, clock2.time)
+    clock1.restart
+    assert_operator(clock1.time, :<=, clock2.time)
+    refute_operator(clock1.time, :>, clock2.time)
+    assert_operator(clock2.time, :>, clock1.time)
   end
   
   class MyClock < Clock
@@ -63,11 +63,10 @@ class TestClock < Test::Unit::TestCase
   def test_subclass
     my_clock = MyClock.new
     
-    assert_in_delta(my_clock.time, 0, 10)
+    assert_in_delta(my_clock.time.as_milliseconds, 0, 10)
     assert_equal(my_clock.class, my_clock.dup.class)
     
-    assert_match(/TestClock::MyClock\(\d+ms\)/, my_clock.inspect)
-    
+    assert_match(/TestClock::MyClock\(\d+\.\d+s\)/, my_clock.inspect)
   end
   
 end
