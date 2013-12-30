af2deb1016aeb4c03edad7a537fe63f51f65842e
Max <silverhammermba@gmail.com>
2013-12-29 23:12:41 -0500

    Rewrite Clock test using MiniTest specs

--- test/System/Clock.rb
+++ test/System/Clock.rb
@@ -1,73 +1,74 @@
+require 'minitest/autorun'
+require 'sfml/audio'
+include SFML
 
-class TestClock < Test::Unit::TestCase
-  include SFML
-  
-  def test_initialization
+describe Clock do
+  before do
+    @clock = Clock.new
+  end
+
+  it "duplicates elapsed time" do
+    sleep 0.1
+    clock = @clock.dup
+    @clock.time.as_milliseconds.must_be_close_to clock.time.as_milliseconds
+    @clock.wont_be_same_as clock
+  end
+
+  it "has a useful string representation" do
+    @clock.inspect.must_match /^SFML::Clock\(\d+\.\d+s\)$/
+    @clock.to_s.must_match /^SFML::Clock\(\d+\.\d+s\)$/
+  end
+
+  it "is accurate" do
+    sleep 0.1
+    @clock.time.as_milliseconds.must_be_close_to 100, 40
+  end
+
+  it "can be reset" do
+    @clock.time.as_milliseconds.must_be_close_to 0, 40
+    sleep 0.1
+    @clock.time.as_milliseconds.must_be_close_to 100, 40
+
+    @clock.restart
+    @clock.time.as_milliseconds.must_be_close_to 0, 40
+    sleep 0.1
+    @clock.time.as_milliseconds.must_be_close_to 100, 40
+  end
+
+  it "is comparable to other Clocks" do
+    sleep 0.1
     clock = Clock.new
+    @clock.must_be :>, clock
+    @clock.wont_be :<=, clock
+
+    sleep 0.1
+    @clock.must_be :>, clock
+    @clock.wont_be :<=, clock
+
+    @clock.reset
+    @clock.must_be :<=, clock
+    @clock.wont_be :>, clock
+    clock.must_be :>, @clock.time
   end
-  
-  def test_equal
-    clock1 = Clock.new
-    sleep(0.100)
-    clock2 = clock1.dup
-    sleep(0.100)
-    clock3 = Clock.new
-    assert_equal(clock1, clock2)
-    refute_equal(clock1, clock3)
-    refute(clock1.eql? clock2)
-    refute(clock1.eql? clock3)
+
+  describe "when subclassed" do
+    before do
+      class MyClock < Clock
+      end
+
+      @clock = MyClock.new
+    end
+
+    it "is accurate" do
+      @clock.time.as_milliseconds.must_be_close_to 0, 10
+    end
+
+    it "can be duplicated" do
+      @clock.class.must_be_same_as @clock.dup.class
+    end
+
+    it "has a useful string representation" do
+      @clock.inspect.must_match /^MyClock\(\d+\.\d+s\)$/
+    end
   end
-  
-  def test_inspect
-    clock = Clock.new
-    assert_match(/SFML::Clock\(\d+ms\)/, clock.inspect)
-    assert_match(/ SFML::Clock\(\d+ms\) /, " #{clock} ")
-  end
-  
-  def test_elapsed_time
-    clock = Clock.new
-    sleep(0.100)
-    assert_in_delta(clock.time, 100, 40)
-  end
-  
-  def test_reset
-    clock = Clock.new
-    assert_in_delta(clock.time, 0, 40)
-    sleep(0.100)
-    assert_in_delta(clock.time, 100, 40)
-    
-    clock.reset
-    assert_in_delta(clock.time, 0, 40)
-    sleep(0.100)
-    assert_in_delta(clock.time, 100, 40)
-  end
-  
-  def test_compare
-    clock1 = Clock.new
-    sleep(0.100)
-    clock2 = Clock.new
-    assert_operator(clock1, :>, clock2)
-    refute_operator(clock1, :<=, clock2)
-    sleep(0.100)
-    assert_operator(clock1, :>, clock2)
-    refute_operator(clock1, :<=, clock2)
-    clock1.reset
-    assert_operator(clock1, :<=, clock2)
-    refute_operator(clock1, :>, clock2)
-    assert_operator(clock2, :>, clock1.time)
-  end
-  
-  class MyClock < Clock
-  end
-  
-  def test_subclass
-    my_clock = MyClock.new
-    
-    assert_in_delta(my_clock.time, 0, 10)
-    assert_equal(my_clock.class, my_clock.dup.class)
-    
-    assert_match(/TestClock::MyClock\(\d+ms\)/, my_clock.inspect)
-    
-  end
-  
 end
