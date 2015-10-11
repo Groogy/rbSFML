aad5a2f97774f743eed6cfaa11b1b7900b3be962
Groogy <groogy@groogy.se>
2015-10-11 15:25:34 +0200

    Added equal comparison test and duplication test for SFML::Time

--- spec/time_spec.rb
+++ spec/time_spec.rb
@@ -1,5 +1,37 @@
 require './lib/rbsfml.so'
 
 describe SFML::Time do
-	
+  describe "compare methods" do
+    context "given an equal time" do
+      time1 = SFML.seconds(2.0)
+      time2 = SFML.seconds(2.0)
+
+      it "returns true on equal" do
+        expect(time1 == time2).to be_truthy
+      end
+
+      it "returns false on less than" do
+        expect(time1 < time2).to be_falsy
+      end
+
+      it "returns false on greater than" do
+        expect(time1 > time2).to be_falsy
+      end
+    end
+  end
+
+  describe "dup" do
+    context "given an initialized time" do
+      original = SFML.seconds(4.0)
+      copy = original.dup
+      
+      it "creates an equal object" do
+        expect(copy == original).to be_truthy
+      end
+
+      it "creates a new object" do
+        expect(copy.equal?(original)).to be_falsy
+      end
+    end
+  end
 end
