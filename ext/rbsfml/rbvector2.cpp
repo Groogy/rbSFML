410ffb754caf0e28e018801ca314eefae266e92d
Groogy <groogy@groogy.se>
2015-10-17 16:29:05 +0200

    Started working on rbVector2

---
+++ ext/rbsfml/rbvector2.cpp
@@ -0,0 +1,84 @@
+/* rbSFML
+ * Copyright (c) 2015 Henrik Valter Vogelius Hansson - groogy@groogy.se
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software in
+ *    a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#include "rbvector2.hpp"
+#include "error.hpp"
+#include "macros.hpp"
+
+namespace 
+{
+	constexpr char symVarX[] = "@x";
+	constexpr char symVarY[] = "@y";
+}
+
+rbVector2Class rbVector2::ourDefinition;
+
+void rbVector2::defineClass(const rb::Value& sfml)
+{
+	ourDefinition = rbVector2Class::defineClassUnder<rb::RubyObjAllocator>("Vector2", sfml);
+	ourDefinition.defineMethod<0>("initialize", &rbVector2::initialize);
+	ourDefinition.defineMethod<1>("initialize_copy", &rbVector2::initializeCopy);
+	ourDefinition.defineMethod<2>("marshal_dump", &rbVector2::marshalDump);
+	ourDefinition.defineMethod<2>("marshal_load", &rbVector2::marshalLoad);
+}
+
+rb::Value rbVector2::initialize(const rb::Value& self, const std::vector<rb::Value>& args)
+{
+	switch( args.size() )
+    {
+        case 0:
+        	self.setVariable<symVarX>(0);
+        	self.setVariable<symVarY>(0);
+            break;
+        case 1:
+        	rbVector2::initializeCopy(self, args[0]);
+            break;
+        case 2:
+        	self.setVariable<symVarX>(args[0]);
+        	self.setVariable<symVarY>(args[0]);
+            break;
+        default:
+            //INVALID_ARGUMENT_LIST( argc, "0..2" );
+    }
+
+	return self;
+}
+
+rb::Value rbVector2::initializeCopy(const rb::Value& self, const rb::Value& value)
+{
+	std::string type = self.getClassName();
+	rb::raise(rb::RuntimeError, "%s can not be copied!", type.c_str());
+	return rb::Nil;
+}
+
+rb::Value rbVector2::marshalDump(const rb::Value& self)
+{
+	std::string type = self.getClassName();
+	rb::raise(rb::TypeError, "can't dump %s", type.c_str());
+	return rb::Nil;
+}
+
+rb::Value rbVector2::marshalLoad(const rb::Value& self)
+{
+	std::string type = self.getClassName();
+	rb::raise(rb::TypeError, "can't dump %s", type.c_str());
+	return rb::Nil;
+}
---
+++ ext/rbsfml/rbvector2.hpp
@@ -0,0 +1,47 @@
+/* rbSFML
+ * Copyright (c) 2015 Henrik Valter Vogelius Hansson - groogy@groogy.se
+ * This software is provided 'as-is', without any express or implied warranty.
+ * In no event will the authors be held liable for any damages arising from
+ * the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software in
+ *    a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ *
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ *
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+#ifndef RBSFML_RBVECTOR2_HPP
+#define RBSFML_RBVECTOR2_HPP
+
+#include <SFML/System/Vector2.hpp>
+#include "class.hpp"
+#include "object.hpp"
+
+class rbVector2;
+
+typedef rb::Class<rbVector2> rbVector2Class;
+
+class rbVector2
+{
+public:
+	static void defineClass(const rb::Value& sfml);
+
+	static rb::Value initialize(const rb::Value& self, const std::vector<rb::Value>& args);
+	static rb::Value initializeCopy(const rb::Value& self, const rb::Value& value);
+	static rb::Value marshalDump(const rb::Value& self);
+	static rb::Value marshalLoad(const rb::Value& self);
+
+private:
+	static rbVector2Class ourDefinition;
+};
+
+#endif // RBSFML_RBVECTOR2_HPP
