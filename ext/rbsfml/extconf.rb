396a012e4e45060f17d7d0c4d6f0f1844da05425
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2015-10-18 15:20:04 +0200

    Fixed linkage errors and made sure everything is linked

--- ext/rbsfml/extconf.rb
+++ ext/rbsfml/extconf.rb
@@ -12,14 +12,26 @@ extension_name = 'rbsfml'
 # Try and find SFML
 HEADER_DIRS = [
   "C:/Program Files (x86)/SFML/include",
+  "C:/Program Files (x86)/glew/include",
 ]
 LIB_DIRS = [
   "C:/Program Files (x86)/SFML/lib",
+  "C:/Program Files (x86)/glew/lib",
 ]
 
 dir_config("config", HEADER_DIRS, LIB_DIRS)
 
+have_library("freetype") or raise "Need supported version freetype!"
+have_library("jpeg") or raise "Need supported version jpeg!"
+have_library("glew32") or raise "Need supported version glew!"
+have_library("gdi32") or raise "Need supported version gdi32!"
+have_library("opengl32") or raise "Need supported version opengl32!"
+have_library("openal32") or raise "Need supported version openal32!"
+have_library("winmm") or raise "Need supported version winmm!"
 have_library("sfml-system-s") or raise "Need supported version of sfml-system!"
+have_library("sfml-window-s") or raise "Need supported version of sfml-window!"
+have_library("sfml-graphics-s") or raise "Need supported version of sfml-graphics!"
+have_library("sfml-audio-s") or raise "Need supported version of sfml-audio!"
 
 create_header
 create_makefile(extension_name)
