ba81be3650651036ba1ca9e46ed10762bd9351a3
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2015-10-18 15:31:52 +0200

    Updated clock spec o use sfml sleep and to not be super long

--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -4,10 +4,10 @@ describe SFML::Clock do
   describe "will give elapsed time" do
     context "after two seconds has passed" do
       clock = SFML::Clock.new()
-      sleep(2)
+      SFML.sleep(SFML.milliseconds(20))
 
       it "the returned time will be longer" do
-        expect(clock.elapsed_time.as_seconds).to be > 2.0
+        expect(clock.elapsed_time.as_milliseconds).to be > 20
       end
     end
   end
@@ -15,11 +15,11 @@ describe SFML::Clock do
   describe "will reset its time" do
     context "after restart has been called" do
       clock = SFML::Clock.new()
-      sleep(2)
+      SFML.sleep(SFML.milliseconds(20))
       time = clock.restart
 
       it "returning the elapsed time up to that point" do
-        expect(time.as_seconds).to be > 2.0
+        expect(time.as_milliseconds).to be > 18
       end
 
       it "next call to elapsed_time give a lower elapsed time" do
