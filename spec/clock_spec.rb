4b4381e3c796566d7ab2de288881052ca763775b
Groogy <groogy@groogy.se>
2015-10-23 00:15:33 +0200

    Fixed error in spec for clock

--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -3,7 +3,7 @@ require './lib/rbsfml.so'
 describe SFML::Clock do
   describe "will give elapsed time" do
     context "after two seconds has passed" do
-      clock = SFML::Clock.new()
+      clock = SFML::Clock.new
       SFML.sleep(SFML.milliseconds(20))
 
       it "the returned time will be longer" do
@@ -14,9 +14,12 @@ describe SFML::Clock do
 
   describe "will reset its time" do
     context "after restart has been called" do
-      clock = SFML::Clock.new()
-      SFML.sleep(SFML.milliseconds(20))
-      time = clock.restart
+      clock = SFML::Clock.new
+      time = SFML::Time.new
+      before(:each) do
+        SFML.sleep(SFML.milliseconds(20))
+        time = clock.restart
+      end
 
       it "returning the elapsed time up to that point" do
         expect(time.as_milliseconds).to be > 18 # SFML doesn't guarantee that elapsed time will be passed 20 milliseconds
